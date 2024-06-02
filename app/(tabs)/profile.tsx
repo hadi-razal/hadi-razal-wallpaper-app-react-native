32035e3b61647ab9fd1ba19c919eda1ba8b9bce3
hadi-razal <hadhirasal22@gmail.com>
2024-06-03 01:17:13 +0530

    updates

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,29 @@
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function Page() {
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Profile Page</Text>
-            <TextInput placeholder='Email' style={{ backgroundColor: "#EDEFF0", width: 300, padding: 3, borderRadius: 5, paddingVertical: 10, shadowColor: 'gray', shadowOpacity: 50, shadowOffset: { width: 5, height: 5 }, color: "black" }} />
+            <View style={styles.inputContainer}>
+                <TextInput
+                    placeholder='Email'
+                    placeholderTextColor="#000000" // Change this to your desired color
+                    style={styles.input}
+                />
+                <TextInput
+                    placeholder='Email'
+                    placeholderTextColor="#000000" // Change this to your desired color
+                    style={styles.input}
+                />
+                <TextInput
+                    placeholder='Email'
+                    placeholderTextColor="#000000" // Change this to your desired color
+                    style={styles.input}
+                />
+                <TouchableOpacity style={styles.btn}>
+                    <Button color={"white"} title='Submit' />
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
@@ -15,9 +34,13 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'flex-start',
     },
+    input: {
+        backgroundColor: "#d9d9d9", width: 300, paddingHorizontal: 7, borderRadius: 5, paddingVertical: 20
+    },
     title: {
-        fontSize: 50,
+        fontSize: 30,
         fontWeight: 'bold',
+        paddingVertical: 10,
     },
     separator: {
         marginVertical: 30,
@@ -30,5 +53,14 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         color: '#fff',
         marginLeft: 10,
-    },
+    }, inputContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 10,
+    }, btn: {
+        paddingHorizontal: 10,
+        paddingVertical: 10,
+        backgroundColor: '#793BEC',
+        borderRadius: 10,
+    }
 });
